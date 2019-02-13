7b0da24c8ef83a1de87e9f1ae052e7eabb5ff40d
William Jiang <williamjxj@hotmail.com>
2019-02-13 14:46:57 +0800

    add client/buildls

---
+++ client/build/precache-manifest.71110c9760adc2dcebaa4ebd29bb642b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ef2109f1200d103b1615",
+    "url": "/static/js/main.ef2109f1.chunk.js"
+  },
+  {
+    "revision": "db23599a0c4eb77cba04",
+    "url": "/static/js/2.db23599a.chunk.js"
+  },
+  {
+    "revision": "ef2109f1200d103b1615",
+    "url": "/static/css/main.88614744.chunk.css"
+  },
+  {
+    "revision": "a612041c5d9e2165a46a3ce72f9f8f7c",
+    "url": "/index.html"
+  }
+];
