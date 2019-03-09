f4500cfa12c09d265e4803bbb5e52996ca2d9260
William Jiang <William_Jiang@epam.com>
2019-03-09 16:28:14 +0800

    remove bin/www, add SZ as a select option.

--- server.js
+++ server.js
@@ -155,4 +155,46 @@ app.use((err, req, res, next) => {
   });
 });
 
+var debug = require('debug')('user-login:server');
+var http = require('http');
+var port = process.env.PORT || 8888;
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+
 module.exports = app;
